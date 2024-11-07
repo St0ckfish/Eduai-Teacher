54fd554a719a35f424e1c327cbe2a97366f154f6
M0staphaTaha <darsh.taha12382@gmail.com>
2024-11-07 15:14:24 +0200

    finish auth pages

--- src/app/textbooks/french/page.tsx
+++ src/app/textbooks/french/page.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link";
 import Container from "~/_components/Container";
 
-const Textbooks = () => {
-    return (  
+const French = () => {
+    return ( 
         <Container>
             <div className="flex w-full items-center justify-start gap-5">
-                <Link href="/textbooks/" className="rounded-xl p-6 bg-white border border-primary grid gap-2">
+                <Link href="/textbooks/" className="rounded-xl p-6 bg-white grid gap-2">
                     <h1 className="font-semibold text-xl">English</h1>
                     <p><span className="font-medium text-primary">4</span> Number of grades </p>
                 </Link>
-                <Link href="/textbooks/french" className="rounded-xl p-6 bg-white grid gap-2">
+                <Link href="/textbooks/french" className="rounded-xl p-6 bg-white border border-primary grid gap-2">
                     <h1 className="font-semibold text-xl">French</h1>
                     <p><span className="font-medium text-primary">2</span> Number of grades </p>
                 </Link>
@@ -18,7 +18,7 @@ const Textbooks = () => {
 
             </div>
         </Container>
-    );
+     );
 }
  
-export default Textbooks;
+export default French;
